df276a279ffce601b6eb791ee863940c675dfe28
nahiduzzaman2021 <akash.nahid.27@gmail.com>
2022-03-02 02:03:46 +0600

    7th commit - error handle done

--- js/mobile-corner-app.js
+++ js/mobile-corner-app.js
@@ -1,16 +1,24 @@
+// Toogle Spinner
+const toggleSpinner = displayStyle => {
+    document.getElementById('loading').style.display = displayStyle;
+};
+
+
+// All product
 const allMobile = () => {
 
     const searchBox = document.getElementById('search-field');
     const searchValue = searchBox.value.toLowerCase();
-    console.log((searchValue.value))
-    console.log(searchValue)
 
     if (searchValue === '') {
         const warningContainer = document.getElementById('warning-container');
         warningContainer.style.display = 'block';
     }
     else {
-        // All Mobile search fetch
+        // display Spinner
+        toggleSpinner('block');
+
+        // product fetch
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
         fetch(url)
             .then(res => res.json())
@@ -27,17 +35,17 @@ const allMobile = () => {
 
 // Display result
 const displayMobile = mobiles => {
+
     if (mobiles.length == 0) {
         const warningContainer2 = document.getElementById('warning-container2');
         warningContainer2.style.display = 'block';
     }
+
     document.getElementById('search-field').value = '';
     const parentDiv = document.getElementById('mobiles-container');
     const max20MobileData = mobiles.slice(0, 20)
 
     max20MobileData.forEach(mobile => {
-        console.log(mobile)
-
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML =
@@ -53,28 +61,31 @@ const displayMobile = mobiles => {
                     <button onclick="details('${mobile.slug}')" class="btn btn-outline-success">Explore</button> <i class="text-success fa-solid fa-location-arrow arrow-icon"></i>
                 </div>
             </div>
-        
+
+           
         
         `
-        parentDiv.appendChild(div)
-
+        parentDiv.appendChild(div);
+        toggleSpinner('none');
     })
+
 };
 
 const details = info => {
+    // display Spinner
+    toggleSpinner('block');
     document.getElementById('details-container').textContent = '';
     const url = `https://openapi.programming-hero.com/api/phone/${info}`
     fetch(url)
         .then(res => res.json())
         .then(data => setDetails(data.data))
-    // document.getElementById('mobiles-container').textContent = '';
+
 };
 
 const setDetails = info => {
     const detailsDiv = document.getElementById('details-container');
     detailsDiv.style.display = 'block';
     const div = document.createElement('div');
-    console.log(info)
     div.innerHTML =
         `
         <div class="card mb-3 shadow-sm p-3 mb-5 bg-body rounded">
@@ -97,73 +108,73 @@ const setDetails = info => {
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">First Release:</span>
-                                <span>${info.releaseDate ? info.releaseDate : 'Not found'}</span>
+                                <span>${info?.releaseDate ? info.releaseDate : 'Not found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">Storage:</span>
-                                <span>${info.mainFeatures.storage ? info.mainFeatures.storage : 'Not Found'}</span>
+                                <span>${info?.mainFeatures?.storage ? info.mainFeatures.storage : 'Not Found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">Display Size:</span>
-                                <span>${info.mainFeatures.displaySize ? info.mainFeatures.displaySize : 'Not Found'}</span>
+                                <span>${info?.mainFeatures?.displaySize ? info.mainFeatures.displaySize : 'Not Found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">Chipset:</span>
-                                <span>${info.mainFeatures.chipSet ? info.mainFeatures.chipSet : 'Not Found'}</span>
+                                <span>${info?.mainFeatures?.chipSet ? info.mainFeatures.chipSet : 'Not Found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">Memory:</span>
-                                <span>${info.mainFeatures.memory ? info.mainFeatures.memory : 'Not Found'}</span>
+                                <span>${info?.mainFeatures?.memory ? info.mainFeatures.memory : 'Not Found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
-                                <span class="fw-bold">Sensor: </span>
-                                <span>${info.mainFeatures.sensors ? info.mainFeatures.sensors : 'Not Found'}</span>
+                                <span class="fw-bold">Sensor: </span> <br>
+                                <span class="text-center">${info?.mainFeatures?.sensors ? info.mainFeatures.sensors : 'Not Found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">WLAN:</span>
-                                <span>${info.others.WLAN ? info.others.WLAN : 'Not Found'}</span>
+                                <span>${info?.others?.WLAN ? info.others.WLAN : 'Not Found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">Bluetooth:</span>
-                                <span>${info.others.Bluetooth ? info.others.Bluetooth : 'not found'}</span>
+                                <span>${info?.others?.Bluetooth ? info.others.Bluetooth : 'not found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">GPS:</span>
-                                <span>${info.others.GPS ? info.others.GPS : 'Not Found'}</span>
+                                <span>${info?.others?.GPS ? info.others.GPS : 'Not Found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">NFC:</span>
-                                <span>${info.others.NFC ? info.others.NFC : 'Not Found'}</span>
+                                <span>${info?.others?.NFC ? info.others.NFC : 'Not Found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">Radio:</span>
-                                <span>${info.others.Radio ? info.others.NFC : 'Not Found'}</span>
+                                <span>${info?.others?.Radio ? info.others.NFC : 'Not Found'}</span>
                             </p>    
                         </li>
                         <li class="list-group-item">
                             <p class="list-span"> 
                                 <span class="fw-bold">USB:</span>
-                                <span>${info.others.USB ? info.others.USB : 'Not Found'}</span>
+                                <span>${info?.others?.USB ? info.others.USB : 'Not Found'}</span>
                             </p>    
                         </li>
                 
@@ -177,28 +188,40 @@ const setDetails = info => {
 
     `
     detailsDiv.appendChild(div);
+    toggleSpinner('none');
 
 };
 
 // error closed button
 let close = document.getElementById("close-btn");
 close.addEventListener('click', function () {
-
     const warningContainer = document.getElementById('warning-container');
     warningContainer.style.display = 'none';
+    toggleSpinner('none');
 
-})
+});
+
 // 2nd error closed button
 let close2 = document.getElementById("close-btn2");
 close2.addEventListener('click', function () {
-
     const warningContainer2 = document.getElementById('warning-container2');
     warningContainer2.style.display = 'none';
+    toggleSpinner('none');
 
-})
+});
+
 // info closed button closed 
 function InfoClosed() {
-    console.log('closed click')
     const detailsDiv = document.getElementById('details-container');
     detailsDiv.style.display = 'none';
-}
+    toggleSpinner('none');
+};
+
+// faq modal
+
+let myModal = document.getElementById('myModal')
+let myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+})
